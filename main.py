48b0e46951954e171488b15f0b26beb337c161d7
asjoseph <34357308+asjoseph@users.noreply.github.com>
2017-12-21 20:31:59 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from caesar import rotate_string
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+
+form="""
+<!DOCTYPE html>
+<html>
+    <head>
+    <title> Adelias Web Caesar </title>
+        <style>
+            
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius:10px;
+            }}
+            textarea {{
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }}
+        </style>
+    </head>
+
+<body>
+        <form method='POST'>
+            Rotate by: <input type="text" name="rot" />
+            <br>
+            <br>
+            Your Text: <textarea name="text">{0}</textarea>
+            <br>
+            <input type="submit" />
+        </form>
+</body>
+</html>
+"""
+
+@app.route('/', methods = ['POST'])
+def encrypt():
+    rot = int(request.form["rot"])
+    text = request.form["text"]
+    return form.format(rotate_string(text,rot))
+
+@app.route('/')
+def index():
+    return form.format("")
+
+app.run()
